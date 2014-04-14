df66b67f081cb173aaf1557107a13ceef46b7306
Joost Diepenmaat <joost@moneybird.com>
2014-04-14 22:35:08 +0200

    Translation helper

--- lib/i18n/workflow.rb
+++ lib/i18n/workflow.rb
@@ -2,6 +2,7 @@ require "i18n/workflow/version"
 require "i18n/workflow/always_cascade"
 require "i18n/workflow/exception_handler"
 require "i18n/workflow/explicit_scope_key"
+require "i18n/workflow/translation_helper"
 
 module I18n
   module Workflow
