dd248333214054043ccf1a15063c37823b8fdbc3
Edwin Vlieg <edwin@moneybird.com>
2017-06-06 14:53:55 +0200

    Cleanup modules

--- lib/i18n/workflow/translation_helper.rb
+++ lib/i18n/workflow/translation_helper.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-module NeverRaiseI18nException
-  def translate(key, options = {})
-    super(key, options.merge(raise: false))
+module I18n
+  module Workflow
+    module NeverRaiseI18nException
+      def translate(key, options = {})
+        super(key, options.merge(raise: false))
+      end
+    end
   end
 end
 
-ActionView::Helpers::TranslationHelper.prepend(NeverRaiseI18nException) if defined?(ActionView)
+ActionView::Helpers::TranslationHelper.prepend(I18n::Workflow::NeverRaiseI18nException) if defined?(ActionView)
