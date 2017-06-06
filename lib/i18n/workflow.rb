70d74b80d1dfba07f038c3f4dfe0be08c49f63ee
Edwin Vlieg <edwin@moneybird.com>
2017-06-06 17:06:38 +0200

    Remove require to removed file

--- lib/i18n/workflow.rb
+++ lib/i18n/workflow.rb
@@ -4,7 +4,6 @@ require "i18n/workflow/version"
 require "i18n/workflow/always_cascade"
 require "i18n/workflow/exception_handler"
 require "i18n/workflow/explicit_scope_key"
-require "i18n/workflow/translation_helper"
 
 module I18n
   module Workflow
